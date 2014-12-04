097b865de8a3d0895cb1a107081423a697b909b1
Sohair Ahmad <sohair.ahmad@square63.com>
2014-12-04 09:26:39 +0500

    Added alias to serialize field

--- lib/n_attributes.rb
+++ lib/n_attributes.rb
@@ -5,4 +5,23 @@ module NAttributes
   def self.ipsum
     "Lorem ipsum dolor sit amet, consectetur adipisicing ...."
   end
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def n_attributes(*args)
+      args.each do |arg|
+        serialize %(:#{arg})
+      end
+    end
+
+    def inherited(subclass)
+      @n_attributes.each do |n_attrib|
+        instance_var = "@#{n_attrib}"
+        subclass.instance_variable_set(instance_var, instance_variable_get(instance_var))
+      end
+    end
+  end
 end
--- lib/n_attributes/version.rb
+++ lib/n_attributes/version.rb
@@ -1,3 +1,3 @@
 module NAttributes
-  VERSION = "0.1.0"
+  VERSION = "0.1.2"
 end
