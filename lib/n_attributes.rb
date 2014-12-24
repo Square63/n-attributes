4a836679746b338cb6f9ccd8f3d12e4d237be652
Sohair Ahmad <sohair.ahmad@square63.com>
2014-12-24 10:26:44 +0500

    Implemented dot accessibility with monkey patch help

--- lib/n_attributes.rb
+++ lib/n_attributes.rb
@@ -1,50 +1,31 @@
+# encoding: utf-8
 require "n_attributes/version"
 
-module NAttributes
-  require 'n_attributes/railtie' if defined?(Rails)
+class Object
+  def n_attribute_keys(field, *names)
+    names.delete(names[0])
+    names.each do |key|
+      define_method("#{key}=") do |value|
+        self.send(field)["#{key}"] = value
+        self.save
+      end
 
+      define_method("#{key}") do
+        self.send(field)["#{key}"]
+      end
+    end
+  end
+end
+
+module NAttributes
   def self.included(base)
     base.extend(ClassMethods)
   end
 
   module ClassMethods
-    def n_attributes(*args)
-      args.each do |arg|
-        serialize %(:#{arg}), Hash
-      end
-    end
-  end
-
-  def method_missing(method, *args, &block)
-    if method.present?
-      run_split(method, *args, &block)
-    else
-      super
-    end
-  end
-
-  def run_split(method, *args, &block)
-    method = method.to_s
-    attribute, key = method.gsub('=', '').split('_')
-    if args[0].nil?
-      get_value(attribute, key)
-    else
-      set_key(attribute, key, args[0])
-    end
-  end
-
-  def set_key(field, key, value)
-    if self.send(field).nil?
-      self.send(field+ '=', Hash[key, value].to_s)
-    else
-      self.send(field+ '=', eval(self.send(field)).merge(Hash[key, value]).to_s)
-    end
-    self.save
-  end
-
-  def get_value(field, key)
-    if key.present?
-      eval(self.send(field))[key]
+    def n_attribute_keys(*names)
+      serialize names[0], Hash
+      Object.n_attribute_keys(names[0], *names)
     end
   end
 end
